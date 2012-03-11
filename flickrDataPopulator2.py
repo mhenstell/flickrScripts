945e5a6d503ee44c08dd9f1265e5035a0cc0119f
Max Henstell <max@Totoro.local>
2012-03-10 22:05:47 -0500

    fdp2 cleanup

--- flickrDataPopulator2.py
+++ flickrDataPopulator2.py
@@ -6,35 +6,26 @@ from multiprocessing import Pool
 
 class FlickrDataDownloader:
 
-	children = {}
-	jobs = []					# current list of queued jobs
-
-	def __init__(self, flickrInstance, redisInstance, workers=8):
+	def __init__(self, flickrInstance, redisInstance, workers=4):
 		self.flickrInstance = flickrInstance
 		self.redisInstance = redisInstance
 		self.workers = workers
 
 	def run(self):
 
-		ids = redisInstance.smembers("infoQueue")
-
+		ids = self.redisInstance.smembers("infoQueue")
 		pool = Pool(processes = self.workers)
-		print pool.map(retrieveDataForID, ids)
+		print pool.map(self.retrieveDataForID, ids)
 
-	def retrieveDataForID(self, id, flickrInstance, redisInstance):
+	def retrieveDataForID(self, id):
 
 		print "Retriving data for %s" % id
 
-		exif = flickrInstance.photos_getExif(photo_id=id, format="json").replace("jsonFlickrApi(", "")[0:-1]
-		info = flickrInstance.photos_getInfo(photo_id=id, format="json").replace("jsonFlickrApi(", "")[0:-1]
+		exif = self.flickrInstance.photos_getExif(photo_id=id, format="json").replace("jsonFlickrApi(", "")[0:-1]
+		info = self.flickrInstance.photos_getInfo(photo_id=id, format="json").replace("jsonFlickrApi(", "")[0:-1]
 
-		redisInstance.set(id + ":info", info)
-		redisInstance.set(id + ":exif", exif)
-		redisInstance.srem("infoQueue", id)
+		self.redisInstance.set(id + ":info", info)
+		self.redisInstance.set(id + ":exif", exif)
+		self.redisInstance.srem("infoQueue", id)
 
 		return True
-	
-	
-	
-	
-	
