5a5e5ffb4b4044398a3404d2766205b53360618f
Max Henstell <max@kapamaki.net>
2012-03-11 00:46:43 -0500

    working downloader module

--- flickrDownloader.py
+++ flickrDownloader.py
@@ -1,83 +1,97 @@
 import os
 import sys
 import redis
-import operator
 import json
 
-children = {}
-maxjobs = 8					# maximum number of concurrent jobs
-jobs = []					# current list of queued jobs
 
-# Default wget options to use for downloading each URL
-wget = ["wget", "-q", "-nd", "-np", "-c", "-r"]
 
-r = redis.StrictRedis(host='localhost', port=6379, db=0)
+class FlickrDownloader:
 
-# Spawn a new child from jobs[] and record it in children{} using
-# its PID as a key.
-def spawn(cmd, *args):
-	argv = [cmd] + list(args)
-	pid = None
-	try:
-		pid = os.spawnlp(os.P_NOWAIT, cmd, *argv)
-		children[pid] = {'pid': pid, 'cmd': argv}
-	except Exception, inst:
-		print "'%s': %s" % ("\x20".join(argv), str(inst))
-	print "spawned pid %d of nproc=%d njobs=%d for '%s'" % \
-		(pid, len(children), len(jobs), "\x20".join(argv))
-	return pid
+	children = {}
+	jobs = []					# current list of queued jobs
+	
+	# Default wget options to use for downloading each URL
+	wget = ["wget", "-q", "-nd", "-np", "-c", "-r"]
+	
+	def __init__(self, redisInstance, workers=8):
+		self.redisInstance = redisInstance
+		self.maxjobs = workers
 
-if __name__ == "__main__":
-	
-	dateTaken = sys.argv[1]
-	
-	try:
-		os.mkdir(dateTaken)
-	except OSError:
-		pass
+	# Spawn a new child from jobs[] and record it in children{} using
+	# its PID as a key.
+	def spawn(self,cmd, *args):
+		argv = [cmd] + list(args)
+		pid = None
+		try:
+			pid = os.spawnlp(os.P_NOWAIT, cmd, *argv)
+			self.children[pid] = {'pid': pid, 'cmd': argv}
+		except Exception, inst:
+			print "'%s': %s" % ("\x20".join(argv), str(inst))
+		print "spawned pid %d of nproc=%d njobs=%d for '%s'" % \
+			(pid, len(self.children), len(self.jobs), "\x20".join(argv))
+		return pid
+
+	def run(self, date):
 		
-	os.chdir(dateTaken)
-	
-	ids = r.smembers("dateTakenIDs:" + dateTaken)
-	
-	urls = []
-	
-	for id in ids:
-		info = r.get(id + ":info")
-		info = json.loads(info)
-		
-		secret = info["photo"]["secret"]
-		server = info["photo"]["server"]
-		farm = info["photo"]["farm"]
-		
-		url = "http://farm%s.static.flickr.com/%s/%s_%s.jpg" % (farm, server, id, secret)
-		
-		urls.append(url)
-		
-	# Build a list of wget jobs, one for each URL in our input file(s).
-	for url in urls:
-		if url is not None:
-			cmd = wget + [url]
-			jobs.append(cmd)
+		dir = date
+
+		try:
+			os.mkdir(dir)
+		except OSError:
+			pass
 			
-	print "%d wget jobs queued" % len(jobs)
+		os.chdir(dir)
 
-	# Spawn at most maxjobs in parallel.
-	while len(jobs) > 0 and len(children) < maxjobs:
-		cmd = jobs[0]
-		if spawn(*cmd):
-			del jobs[0]
-	print "%d jobs spawned" % len(children)
+		urls = []
+		
+		ids = self.redisInstance.zrange("picturesbytimestamp:%s" % date, 0, -1, withscores=True)
 
-	# Watch for dying children and keep spawning new jobs while
-	# we have them, in an effort to keep <= maxjobs children active.
-	while len(jobs) > 0 or len(children):
-		(pid, status) = os.wait()
-		print "pid %d exited. status=%d, nproc=%d, njobs=%d, cmd=%s" % \
-			(pid, status, len(children) - 1, len(jobs), \
-			 "\x20".join(children[pid]['cmd']))
-		del children[pid]
-		if len(children) < maxjobs and len(jobs):
-			cmd = jobs[0]
-			if spawn(*cmd):
-				del jobs[0]
+		for idtuple in ids:
+			
+			id = idtuple[0]
+			timestamp = int(idtuple[1])
+
+			#try:
+			info = self.redisInstance.hget("picturedata:%s" % date, id + ":info")
+			info = json.loads(info)
+			
+			secret = info["photo"]["secret"]
+			server = info["photo"]["server"]
+			farm = info["photo"]["farm"]
+			
+			url = "http://farm%s.static.flickr.com/%s/%s_%s.jpg" % (farm, server, id, secret)
+			#urls.append(url)
+
+			cmd = self.wget + ["-O", "%s.jpg" % timestamp, url]
+			self.jobs.append(cmd)
+
+			#except:
+			#	print "Error getting picture info for %s" % id
+			
+		# Build a list of wget jobs, one for each URL in our input file(s).
+		#for url in urls:
+		#	if url is not None:
+		#		cmd = self.wget + ["-O", "[url]
+		#		self.jobs.append(cmd)
+				
+		print "%d wget jobs queued" % len(self.jobs)
+
+		# Spawn at most maxjobs in parallel.
+		while len(self.jobs) > 0 and len(self.children) < self.maxjobs:
+			cmd = self.jobs[0]
+			if self.spawn(*cmd):
+				del self.jobs[0]
+		print "%d jobs spawned" % len(self.children)
+
+		# Watch for dying children and keep spawning new jobs while
+		# we have them, in an effort to keep <= maxjobs children active.
+		while len(self.jobs) > 0 or len(self.children):
+			(pid, status) = os.wait()
+			print "pid %d exited. status=%d, nproc=%d, njobs=%d, cmd=%s" % \
+				(pid, status, len(self.children) - 1, len(self.jobs), \
+				 "\x20".join(self.children[pid]['cmd']))
+			del self.children[pid]
+			if len(self.children) < self.maxjobs and len(self.jobs):
+				cmd = self.jobs[0]
+				if self.spawn(*cmd):
+					del self.jobs[0]
