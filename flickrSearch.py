87accb2e41f11ce635094653917db94278f3036a
Max Henstell <max@Totoro.local>
2012-03-10 21:17:13 -0500

    flickrSearch module

--- flickrSearch.py
+++ flickrSearch.py
@@ -3,8 +3,42 @@ import redis
 import sys
 
 
-minDate = sys.argv[1]
-maxDate = sys.argv[2]
+class FlickrSearch:
+	
+	def __init__(self, minDate, maxDate, woe_id, redisInstance, flickrCredentials):
+		self.minDate = minDate
+		self.maxDate = maxDate
+		self.woe_id = woe_id
+		self.redisInstance = redisInstance
+
+	def run(self):
+
+		print "Performing Flickr search for photos in woe_id %s between %s and %s" % (self.woe_id, self.minDate, self.maxDate)
+
+		flickr = flickrapi.FlickrAPI(api_key, api_secret)
+
+		currentPage = 1
+		totalPages = 0
+	
+		while True:
+
+			print "\tPopulating Page %s of %s" % (currentPage, totalPages)
+			search = flickr.photos_search(api_key=flickrCredentials['key'], min_taken_date=self.minDate, max_taken_date=self.maxDate, has_geo=True, woe_id=self.woe_id, page=currentPage - 1, format='json').replace("jsonFlickrApi(", "")[0:-1]
+			search = json.loads(search)
+			
+			totalPages = search["photos"]["pages"]
+			photos = search["photos"]["photo"]
+			
+			for photo in photos:
+				id = photo["id"]
+				
+				#Add photo to master picture set
+				redisInstance.sadd("picSet", id)
+
+			currentPage += 1
+			if currentPage > totalPages: break
+
+
 
 if __name__ == "__main__":
 	
